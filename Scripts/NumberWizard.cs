e7d229d141f8bb87b0f43981f38e9aae1336dcaa
Astitva Agarwal <95524228+AstitvaAgarwal@users.noreply.github.com>
2023-05-24 16:28:01 +0530

    Add files via upload

---
+++ Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField]int max;
+    [SerializeField]int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    void Start ()
+    {
+        StartGame();
+	}
+	
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher(){
+        min = guess+1;
+        NextGuess();
+    }
+
+    public void OnPressLower(){
+        max = guess-1;
+        NextGuess();
+    }
+	
+    void NextGuess()
+    {
+        guess = Random.Range(min,max+1); 
+        guessText.text = guess.ToString();
+    }
+}
+
